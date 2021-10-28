fb40e387de3e3005b19ba61c6f8d7560204255a6
FidelSol <fsoloviov2020@mail.ru>
2021-10-28 10:44:15 +0300

    Lesson_1_SoloviovFM

---
+++ DRF/apipoint/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+
+# router
+router.register(r'users',  views.UserViewSet, basename='users')
+
+urlpatterns = [
+    # API
+    path('api/v1/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
